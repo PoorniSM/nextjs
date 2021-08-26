51b611eebfabfc1d73348894ed1d002e0da91633
PoorniSM <85600279+PoorniSM@users.noreply.github.com>
2021-08-26 13:26:37 +0530

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -3,14 +3,6 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 
 const pageNotFound = () => {
-    const router = useRouter();
-
-    useEffect (() => {
-        setTimeout(() => {
-            router.push('/')
-        },3000)
-    }, []);
-
     return ( 
         <div className='not-found'>
             <h1>Page not Found :( </h1>
